70caf974aafe2286f21dc0aa63c5a813e4ed135b
Nik <nik02.maffi@gmail.com>
2025-08-09 17:22:14 +0200

    Added Particles class

---
+++ include/Particles.hpp
@@ -0,0 +1,30 @@
+#ifndef __PARTICLES_HPP__
+#define __PARTICLES_HPP__
+
+#include <xspacewar.hpp>
+
+class GhostParticles;
+
+class Particles {
+private:
+    Vector2 *pos;
+    Vector2 *dir;
+    unsigned int size;
+
+    float flickeringTimer;
+    Color color;
+
+    std::random_device rd;
+	std::uniform_real_distribution<float> dist;
+public:
+    Particles(unsigned int size, const Color &color);
+    ~Particles();
+    Particles &operator=(const Particles &p);
+    void init(const Vector2 &pos);
+    void expandBy(float factor);
+    void draw(void);
+
+    friend class GhostParticles;
+};
+
+#endif //__PARTICLES_HPP__
---
+++ src/Particles.cpp
@@ -0,0 +1,58 @@
+#include <Particles.hpp>
+
+Particles::Particles(unsigned int size, const Color &color):
+pos(new Vector2[size]),
+dir(new Vector2[size]),
+size(size),
+flickeringTimer(.0f),
+color(color),
+dist(-1.f, 1.f) {
+}
+
+Particles::~Particles() {
+    delete[] pos;
+    delete[] dir;
+}
+
+Particles &Particles::operator=(const Particles &p) {
+    pos = p.pos;
+    dir = p.dir;
+    size = p.size;
+    flickeringTimer = p.flickeringTimer;
+    color = p.color;
+	dist = dist;
+
+    return *this;
+}
+
+void Particles::init(const Vector2 &pos) {
+    for(unsigned int i = 0; i < size; i++) {
+        this->pos[i].x = pos.x;
+        this->pos[i].y = pos.y;
+        dir[i].x = dist(rd);
+        dir[i].y = dist(rd);
+    }
+}
+
+void Particles::expandBy(float factor) {
+    for(unsigned int i = 0; i < size; i++) {
+        pos[i].x += dir[i].x * factor;
+        pos[i].y += dir[i].y * factor;
+    }
+}
+
+void Particles::draw(void) {
+    if(flickeringTimer >= FLICKER_FRAME_INTERVAL) {
+        flickeringTimer = .0f;
+
+        if(color.a == 185) {
+            color.a = 255;
+        } else {
+            color.a = 185;
+        }
+    }
+
+    for(unsigned int i = 0; i < size; i++) {
+        DrawPixel(pos[i].x, pos[i].y, color);
+    }
+}
