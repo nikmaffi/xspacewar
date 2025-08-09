fda7d261bd4423e1b5d9d37e5c4e80e2635c7180
Nik <nik02.maffi@gmail.com>
2025-08-09 17:20:31 +0200

    Added Anomaly class

---
+++ include/Anomaly.hpp
@@ -0,0 +1,37 @@
+#ifndef __ANOMALY_HPP__
+#define __ANOMALY_HPP__
+
+#include <xspacewar.hpp>
+#include <Circle.hpp>
+
+class GhostEntity;
+
+class Anomaly {
+private:
+	Vector2 pos;
+	float mass;
+
+	float angle;
+
+	Texture texture;
+    Rectangle sprite;
+    Color color;
+    float scale;
+
+	float flickeringTimer;
+
+	std::random_device rd;
+	std::uniform_real_distribution<float> dist;
+public:
+	Anomaly(const Vector2 &pos, const char *textureRes, float scale = 1.f);
+	~Anomaly();
+	const Vector2 &getPosition(void) const;
+	Circle getCircle(void) const;
+	void changeAnomaly(void);
+	Vector2 attract(float mass, const Vector2 &pos) const;
+	void draw(void);
+
+	friend class GhostEntity;
+};
+
+#endif //__ANOMALY_HPP__
---
+++ src/Anomaly.cpp
@@ -0,0 +1,69 @@
+#include <Anomaly.hpp>
+
+Anomaly::Anomaly(const Vector2 &pos, const char *textureRes, float scale):
+pos(pos),
+mass(1e12),
+angle(.0f),
+texture(LoadTexture(textureRes)),
+sprite((Rectangle){.0f, .0f, (float)texture.width, texture.height / 2.f}),
+color(WHITE),
+scale(scale),
+flickeringTimer(.0f),
+dist(0.f, 359.f) {
+}
+
+Anomaly::~Anomaly() {
+	UnloadTexture(texture);
+}
+
+const Vector2 &Anomaly::getPosition(void) const {
+	return pos;
+}
+
+Circle Anomaly::getCircle(void) const {
+	return Circle(pos, 2);
+}
+
+void Anomaly::changeAnomaly(void) {
+	if(blackHoleAsAnomaly) {
+        sprite = (Rectangle){.0f, texture.height / 2.f, (float)texture.width, texture.height / 2.f};
+	} else {
+        sprite = (Rectangle){.0f, .0f, (float)texture.width, texture.height / 2.f};
+	}
+}
+
+Vector2 Anomaly::attract(float mass, const Vector2 &pos) const {
+	Vector2 dist = (Vector2){this->pos.x - pos.x, this->pos.y - pos.y};
+	float distMag = std::sqrt(dist.x * dist.x + dist.y * dist.y) * WSCALE / WINDOW_WIDTH;
+	float forceMag = G * this->mass * mass / (distMag * distMag * distMag);
+
+	return (Vector2){
+        (forceMag * dist.x / mass) * GetFrameTime() * MULTIPLIER,
+        (forceMag * dist.y / mass) * GetFrameTime() * MULTIPLIER
+    };
+}
+
+void Anomaly::draw(void) {
+	flickeringTimer += GetFrameTime() * flickeringMonitorEffect;
+
+	if(flickeringTimer >= FLICKER_FRAME_INTERVAL) {
+		flickeringTimer = .0f;
+
+		if(color.a == 185) {
+			color.a = 255;
+		} else {
+			color.a = 185;
+		}
+	}
+
+	angle = dist(rd);
+
+    DrawTexturePro(
+        texture,
+        sprite,
+        (Rectangle){pos.x, pos.y, sprite.width * scale, sprite.height * scale},
+        (Vector2){sprite.width / 2.f, sprite.height / 2.f},
+        angle,
+        color
+    );
+}
